d0b213f4ccbce9acecd0c7971dbebc517d04cc98
mahbub-khan <mrkict39@gmail.com>
2025-01-30 11:54:32 +0100

    character level encoding implemented

---
+++ source/SDRDecoder3.7Project/Documentation/Demo/WordEncoder/WordEncoder/WordEncoder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace WordEncoder
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter a sentence: ");
+            string input = Console.ReadLine();
+            Console.WriteLine("Number of characters: " + input.Length);
+
+            StringBuilder encodedString = new StringBuilder();
+
+            foreach (char c in input)
+            {
+                encodedString.Append(((int)c).ToString() + " ");
+            }
+
+            Console.WriteLine("Encoded String: " + encodedString.ToString().Trim());
+            Console.WriteLine("Number of outputs: " + input.Length);
+        }
+    }
+}
