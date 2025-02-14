df51207446e8afbecc6360afd538f322347f63ab
mahbub-khan <mrkict39@gmail.com>
2025-02-15 00:12:15 +0100

    new encoding teschnique applied

--- source/SDRDecoder3.7Project/Documentation/Demo/WordEncoder/WordEncoder/Program.cs
+++ source/SDRDecoder3.7Project/Documentation/Demo/WordEncoder/WordEncoder/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text;
 using System.Linq;
+using System.Collections.Generic;
 
 class Program
 {
@@ -10,43 +11,23 @@ class Program
         string input = Console.ReadLine();
         StringBuilder encodedString = new StringBuilder();
 
-        //ignoring spaces, commas and full-stops for encoding
-        string[] words = input.Split(new char[] { ' ', ',', '.', '?' }, StringSplitOptions.RemoveEmptyEntries);
-        Console.WriteLine("Number of total words: " + words.Length);
-
-        //finding out unique words
+        // Ignoring spaces, commas, and full stops for encoding
+        string[] words = input.Split(new char[] { ' ', ',', '.' }, StringSplitOptions.RemoveEmptyEntries);
         List<string> uniqueWords = words.Select(w => w.ToLower()).Distinct().ToList();
         Console.WriteLine("Number of unique words: " + uniqueWords.Count);
 
         int uniqueWordCount = 0;
-
+        Dictionary<string, float> wordEncoding = new Dictionary<string, float>();
 
         foreach (string uniqueWord in uniqueWords)
         {
-           uniqueWordCount++;
-           Console.WriteLine($" #{uniqueWordCount}: {uniqueWord}");
+            uniqueWordCount++;
+            float encodedValue = uniqueWordCount * 1.0f; // Assigning unique encoding as 1.0, 2.0, etc.
+            wordEncoding[uniqueWord] = encodedValue;
+            encodedString.Append(encodedValue.ToString("F1") + " ");
+            Console.WriteLine($" #{uniqueWordCount}: {uniqueWord} -> Encoded Value: {encodedValue:F1}");
         }
 
-        //New Encoding technique will be implemented here
-
-        
+        Console.WriteLine("Encoded Sentence: " + encodedString.ToString().Trim());
     }
 }
-
-/*
-Current Ouput:
-Enter a sentence: You? you. YOU
-Number of total words: 3
-Number of unique words: 1
- #1: you
-*/
-
-/*
-Previous Output:
-Enter a sentence: You, you, YOU.
-Number of total words: 3
-Number of unique words: 3
- #1: You
- #2: you
- #3: YOU
-*/
