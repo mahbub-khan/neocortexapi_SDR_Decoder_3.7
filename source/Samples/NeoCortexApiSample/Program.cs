08d4878e32d7da407888099f5581b02976e0d083
shovon11509003 <shovon.sfx26@gmail.com>
2025-02-14 17:07:04 +0100

    Single sequence learned with 92.31% accuracy

--- source/Samples/NeoCortexApiSample/Program.cs
+++ source/Samples/NeoCortexApiSample/Program.cs
@@ -73,8 +73,8 @@ namespace NeoCortexApiSample
             //sequences.Add("S1", new List<double>(new double[] { 0.0, 1.0, 0.0, 2.0, 3.0, 4.0, 5.0, 6.0, 5.0, 4.0, 3.0, 7.0, 1.0, 9.0, 12.0, 11.0, 12.0, 13.0, 14.0, 11.0, 12.0, 14.0, 5.0, 7.0, 6.0, 9.0, 3.0, 4.0, 3.0, 4.0, 3.0, 4.0 }));
             //sequences.Add("S2", new List<double>(new double[] { 0.8, 2.0, 0.0, 3.0, 3.0, 4.0, 5.0, 6.0, 5.0, 7.0, 2.0, 7.0, 1.0, 9.0, 11.0, 11.0, 10.0, 13.0, 14.0, 11.0, 7.0, 6.0, 5.0, 7.0, 6.0, 5.0, 3.0, 2.0, 3.0, 4.0, 3.0, 4.0 }));
 
-            sequences.Add("S1", new List<double>(new double[] { 1045.0, 2572.0, 1037.0, 2388.0, 2021.0, 1045.0, 2572.0 }));
-            sequences.Add("S2", new List<double>(new double[] { 2036.0, 1037.0, 2684.0, 1998.0, 1037.0, 2905.0 }));
+            sequences.Add("S1", new List<double>(new double[] { 1045.0, 2572.0, 1037.0, 2388.0, 2021.0, 1045.0, 2572.0, 2036.0, 1037.0, 2684.0, 1998.0, 1037.0, 2905.0 }));
+            //sequences.Add("S2", new List<double>(new double[] { 2036.0, 1037.0, 2684.0, 1998.0, 1037.0, 2905.0 }));
 
 
             //sequences.Add("S1", new List<double>(new double[] { 0.0, 1.0, 0.0, 2.0, 3.0, 4.0, 5.0, 6.0, 5.0, 2.0, 3.0, 7.0, 1.0, 9.0, 12.0, 11.0, 12.0, 13.0, 14.0, 11.0, 12.0 }));
@@ -106,8 +106,8 @@ namespace NeoCortexApiSample
             // Predictor is traversing the list element by element. 
             // By providing more elements to the prediction, the predictor delivers more precise result.
             var list1 = new double[] { 1045.0, 2572.0, 1037.0, 2388.0 };
-            var list2 = new double[] { 2021.0, 1045.0, 2572.0 };
-            var list3 = new double[] { 1684.0, 1098.0, 2037.0 };
+            //var list2 = new double[] { 2021.0, 1045.0, 2572.0 };
+            //var list3 = new double[] { 1684.0, 1098.0, 2037.0 };
 
             //var list1 = new double[] { 1.0, 2.0, 3.0, 4.0, 2.0, 5.0 };
             //var list2 = new double[] { 2.0, 3.0, 4.0 };
@@ -124,11 +124,11 @@ namespace NeoCortexApiSample
             predictor.Reset();
             PredictNextElement(predictor, list1);
 
-            predictor.Reset();
-            PredictNextElement(predictor, list2);
+            //predictor.Reset();
+            //PredictNextElement(predictor, list2);
 
-            predictor.Reset();
-            PredictNextElement(predictor, list3);
+            //predictor.Reset();
+            //PredictNextElement(predictor, list3);
         }
 
         private static void PredictNextElement(Predictor predictor, double[] list)
